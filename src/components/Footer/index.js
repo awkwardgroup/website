0bc696fa9a16fbb2a3190988e705807f114a375f
Joakim Strandell <joakim@awkwardgroup.com>
2020-01-28 16:21:28 +0100

    Updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,11 @@ import styles from "./footer.module.scss"
 
 const Footer = () => (
     <footer className={styles.footer}>
-        Contact Person:<br />
+        <small>Contact Person:<br /></small>
         Joakim Strandell<br />
-        <a href="mailto:joakim@awkwardgroup.com">joakim@awkwardgroup.com</a><br />
         <a href="tel:0707294379">070 729 43 79</a><br />
-        <a href="https://www.linkedin.com/in/joakimstrandell/">LinkedIn</a>
+        <a href="mailto:joakim@awkwardgroup.com">joakim@awkwardgroup.com</a><br />
+        <a href="https://www.linkedin.com/in/joakimstrandell/">LinkedIn</a> / <a href="https://twitter.com/joakimstrandell">Twitter</a>
     </footer>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const Index = () => (
     <SEO />
     <p>Awkward Group is a consultant company based in Stockholm, Sweden.</p>
     <p>Our consultants have extensive experience of working within <i>Business and Process Development</i>, <i>Product and Interface Design</i>, and <i>Full Stack Development</i></p>
-    <p>Please feel free to contact us for any inquiries.</p>
+    <p>Please feel free to contact us if your have any inquiries.</p>
   </Layout>
 )
 
